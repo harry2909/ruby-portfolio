abd4925eaecc30ad396d3b5182c698936adfad86
Harry Preston <hpreston2909@gmail.com>
2026-04-21 09:53:34 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://harry2909.github.io/ruby-portfolio/',
+  site: 'https://harry2909.github.io',
+  base: '/ruby-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
